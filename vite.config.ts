1943e9e48318dc9037a0557281b665c9d034e79c
njordr <njordrenterprises@gmail.com>
2024-11-28 15:34:26 -0600

    fix vite setup, start servers in correct order

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,21 @@ import { defineConfig } from "npm:vite@5.0.12";
 export default defineConfig({
   server: {
     port: 5173,
-    strictPort: true
+    strictPort: true,
+    proxy: {
+      '/views': 'http://localhost:8000',
+      '/cards': 'http://localhost:8000',
+      '/assets': 'http://localhost:8000',
+      '/src/js': 'http://localhost:8000'
+    }
   },
   root: ".",
   build: {
     outDir: "./dist"
+  },
+  resolve: {
+    alias: {
+      '@': '/src'
+    }
   }
 }); 
